b0c1340268a445fd4ac0a2470df52ea0e283d3c4
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-08-06 12:55:59 +0200

    neo4j 3.5

--- start-lds-cluster.sh
+++ start-lds-cluster.sh
@@ -6,7 +6,8 @@ docker stop $(docker ps -f "name=pulsar" -q)
 docker rm $(docker ps -f "name=pulsar" -aq)
 docker run --network=lds-cluster -it --name pulsar -p 6650:6650 -p 8080:8080 -v $PWD/data:/pulsar/data apachepulsar/pulsar:2.4.0 bin/pulsar standalone
 
-docker run --network=lds-cluster -d --name neo4j-lds-cluster -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH=neo4j/PasSW0rd neo4j:3.5.6
+docker pull neo4j:3.5
+docker run --network=lds-cluster -d --name neo4j-lds-cluster -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH=neo4j/PasSW0rd neo4j:3.5
 
 docker stop $(docker ps -q -f "name=lds01" -f "name=lds02" -f "name=lds03") && docker rm $(docker ps -aq -f "name=lds01" -f "name=lds02" -f "name=lds03")
 
